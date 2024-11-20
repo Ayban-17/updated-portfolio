411f5ff9794481aa898d90f13a771295cd5cdb57
Ayban-wbct <mark@webact.com>
2024-11-21 00:36:52 +0800

    changed url

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -33,7 +33,7 @@ export function Projects() {
       name: "Luxury",
       desc: "Luxurious aesthetics with premium feel and sophisticated interactions, ideal for high-end brands and services that want to convey elegance and exclusivity.",
       image: "/assets/projects/luxe.png",
-      liveUrl: "https://landing-pages-designs.netlify.app//luxury",
+      liveUrl: "https://landing-pages-designs.netlify.app/luxury",
       repoUrl: "https://github.com/Ayban-17/landing-pages-designs", 
       category: "Next Js"
     },
@@ -42,7 +42,7 @@ export function Projects() {
       name: "Modern",
       desc: "Dynamic layouts with immersive animations and modern components, designed for contemporary websites that want to showcase innovation and creativity.",
       image: "/assets/projects/modern.png",
-      liveUrl: "https://landing-pages-designs.netlify.app//modern",
+      liveUrl: "https://landing-pages-designs.netlify.app/modern",
       repoUrl: "https://github.com/Ayban-17/landing-pages-designs",
       category: "Next Js"
     },
@@ -51,7 +51,7 @@ export function Projects() {
       name: "Minimalist",
       desc: "Clean, minimalist aesthetic with bold typography and smooth interactions, perfect for modern and sleek web applications that prioritize simplicity and user experience.",
       image: "/assets/projects/minimalist.png", 
-      liveUrl: "https://landing-pages-designs.netlify.app//minimalist",
+      liveUrl: "https://landing-pages-designs.netlify.app/minimalist",
       repoUrl: "https://github.com/Ayban-17/landing-pages-designs",
       category: "Next Js"
     },
